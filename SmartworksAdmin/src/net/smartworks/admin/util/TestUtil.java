c19514cd0a5471fa1d32e117bf81b51ddf178280
gwangminyu <gwangminyu@192.168.0.59>
2013-06-11 10:36:49 +0900

    commit

--- SmartworksAdmin/src/net/smartworks/admin/util/TestUtil.java
+++ SmartworksAdmin/src/net/smartworks/admin/util/TestUtil.java
@@ -5,5 +5,7 @@ public class TestUtil {
 	public static String getName() throws Exception {
 		return "SmartWork.net Admin!!!";
 	}
-	
+	public static String getTestString() throws Exception {
+		return "TEST STRING!!";
+	}
 }
